d9dac646a617a57b1d1bae4d411964517d2c68f3
Nikola <nikolapavlov33@gmail.com>
2019-03-29 03:42:54 +0100

    bad performance when rendering all list elements, should be fixed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from "react";
+import "./App.css";
+import {
+    TextInput,
+    Row,
+    Col,
+    Collection,
+    CollectionItem
+} from "react-materialize";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends PureComponent {
+    state = {
+        value: "",
+        filteredNames: ["Loading names..."]
+    };
+
+    // Getting data from the names.json and setting the state
+    componentDidMount() {
+        const postPromise = fetch("./names.json");
+        postPromise
+            .then(response => {
+                return response.json();
+            })
+            .then(names => {
+                //this.names = names;
+                this.setState({ filteredNames: names });
+            });
+    }
+
+    // Putting the input value inside the state
+    handleChange = e => {
+        const value = e.target.value;
+        this.setState({ value });
+    };
+    render() {
+        return (
+            <div className="App">
+                <TextInput
+                    placeholder="Search Names"
+                    onChange={this.handleChange}
+                />
+                <Row className="name-list-container">
+                    <Col m={12} s={12}>
+                        <Collection>
+                            {/* Bad performance when rendering all items */}
+                            {this.state.filteredNames
+                                .filter(name =>
+                                    name
+                                        .toUpperCase()
+                                        .includes(
+                                            this.state.value.toUpperCase()
+                                        )
+                                )
+                                .map((name, i) => (
+                                    <CollectionItem key={name + i} href="#">
+                                        {name}
+                                    </CollectionItem>
+                                ))}
+                        </Collection>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
 }
 
 export default App;
